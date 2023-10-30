f037eb42027cf65d0e69300150d830efca765caf
HazelBall <roundnround01@gmail.com>
2023-10-30 11:17:06 -0700

    testing public key

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ import { SearchBar } from './components/SearchBar'
 
 const App = () => {
   const [search, setSearch] = useState(null)
+  const [results, setResults] = useState(null)
 
   const submitSearch = (search) => {
     setSearch(search);
+    var httpGet = new XMLHttpRequest();
+    var key = "1DDfqiBG47iHDqDcWjJn26LV5uB4ztETWH2Lj5eR";
+    var url = "https://api.propublica.org/congress/v1/116/both/bills/enacted.json"
+    httpGet.open("GET", url, false);
+    httpGet.setRequestHeader("X-API-Key",key);
+    httpGet.send( null );
+    var newResults = JSON.parse(httpGet.responseText);
+    console.log(newResults);
+    setResults(newResults.results);
   }
 
   return (
@@ -16,7 +26,7 @@ const App = () => {
       </div>
       <SearchBar submitSearch={submitSearch}/>
       {search && <p>
-        {search.input}
+        {results[0].num_results} results
       </p>}
     </>
   )
