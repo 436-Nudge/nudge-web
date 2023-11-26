81209e2826aa019be81de9cfea39da3d3b3613b4
HazelBall <roundnround01@gmail.com>
2023-11-25 17:37:24 -0700

    Added blank response and demographics files

---
+++ src/components/AIResponse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AIResponse = () => {
+  return (
+    <div>AIResponse</div>
+  )
+}
+
+export default AIResponse
---
+++ src/components/SelectDemographics.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectDemographics = () => {
+  return (
+    <div>SelectDemographics</div>
+  )
+}
+
+export default SelectDemographics
