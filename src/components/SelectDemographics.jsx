f470bf2309ab3f7ce84b17f10d6eb97762e7a9c3
HazelBall <roundnround01@gmail.com>
2023-11-25 18:00:51 -0700

    create base form for demographics

--- src/components/SelectDemographics.jsx
+++ src/components/SelectDemographics.jsx
@@ -4,13 +4,26 @@ import React from 'react'
  * Open ended input for users to add their own identities to analyze with the
  * selected legislation.
  * 
- * @param {*} submitDemographics Reference to function that submits the demographics 
- * to python backend so that ai response can be generated.
+ * @param {*} submitDemographics Reference to function that submits the 
+ * demographics as an array of strings to python backend so that ai response
+ * can be generated.
  */
-const SelectDemographics = ({SubmitDemographics}) => {
-  return (
-    <div>SelectDemographics</div>
-  )
+const SelectDemographics = ({submitDemographics}) => {
+    const [demographics, setDemographics] = useState("");
+
+    const submitDemographicInput = (e) => {
+        e.preventDefault();
+        let demographicsArray = demographics.split(',');
+        for(let demographic in demographicsArray) demographic.trim();
+        submitDemographics(demographicsList);
+    }
+    return (
+        <div className="select-demographics">
+            <form onSubmit={(e) => {submitDemographicInput(e)}}>
+                
+            </form>
+        </div>
+    )
 }
 
 export default SelectDemographics
